a09cb6da165bf36f52dd0b9aff40ca6d3ca6e172
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-01 22:28:09 +0530

    Edit

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
+snake_segments = []
 for position in starting_position:
     segment = Turtle(shape="square")
     segment.color("white")
     segment.goto(position)
+    snake_segments.append(segment)
 
 
 
