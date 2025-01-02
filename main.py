79d912408edadb8caea6c5a26d424ef7b506c8a2
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-02 15:18:49 +0530

    Edit

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from turtle import Turtle, Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-snake_segments = []
-for position in starting_position:
-    segment = Turtle(shape="square")
-    segment.color("white")
-    segment.goto(position)
-    snake_segments.append(segment)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 
 
