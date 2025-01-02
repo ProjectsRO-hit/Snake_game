6473d9b38b24fcf61d96601b0b385368085ccced
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-02 23:24:53 +0530

    update

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -12,7 +14,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}",align="center",font=("Courier", 24, "normal"))
+        self.write(f"Score: {self.score}",align=ALIGNMENT,font=FONT)
 
     def increase_score(self):
         self.score += 1
